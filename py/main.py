7f02e14155501a47620d35cd06e3814390174908
Igor Vinokur <vinokuri@yahoo.com>
2021-10-22 14:10:39 +0300

    model fixing

--- py/main.py
+++ py/main.py
@@ -6,37 +6,41 @@ from logic import MoveSimulator
 import time
 import threading
 
-shutdown = False
-sleepTime = 0.1
+import pystan
 
-def systemTime():
-	while not shutdown:
-		SystemClock.instance.incrementTime(sleepTime)
-		time.sleep(sleepTime)
+model = pystan.StanModel(model_code=GMM_MODEL, verbose=False, extra_compile_args=["-w"])
 
-def simulationSubcription(data):
-	print(data)
-
-def mainLoop(robot:GenerativeRobot, world:World):
-	moveSim = MoveSimulator(robot, world)
-	PubSub.instance.subscribe(Topics.simulation, simulationSubcription)
-	moveSim.startRunning(1)
-	simTime = threading.Thread(target=systemTime)
-	simTime.start()
-	simTime.join()
-	# while not shutdown:
-	# 	pass
-
-
-if __name__ == "__main__":
-	print("Init Generative module")
-	robot:GenerativeRobot = GenerativeRobot(State(Location(0, 0), MoveDirection.STAND, 1))
-	w = World.loadWorld()
-	print(w)
-	try:
-		mainLoop(robot, w)
-	except KeyboardInterrupt as ex:
-		print(ex)
-		shutdown = True
-		print('Bye')
+# shutdown = False
+# sleepTime = 0.1
+#
+# def systemTime():
+# 	while not shutdown:
+# 		SystemClock.instance.incrementTime(sleepTime)
+# 		time.sleep(sleepTime)
+#
+# def simulationSubcription(data):
+# 	print(data)
+#
+# def mainLoop(robot:GenerativeRobot, world:World):
+# 	moveSim = MoveSimulator(robot, world)
+# 	PubSub.instance.subscribe(Topics.simulation, simulationSubcription)
+# 	moveSim.startRunning(1)
+# 	simTime = threading.Thread(target=systemTime)
+# 	simTime.start()
+# 	simTime.join()
+# 	# while not shutdown:
+# 	# 	pass
+#
+#
+# if __name__ == "__main__":
+# 	print("Init Generative module")
+# 	robot:GenerativeRobot = GenerativeRobot(State(Location(0, 0), MoveDirection.STAND, 1))
+# 	w = World.loadWorld()
+# 	print(w)
+# 	try:
+# 		mainLoop(robot, w)
+# 	except KeyboardInterrupt as ex:
+# 		print(ex)
+# 		shutdown = True
+# 		print('Bye')
 
